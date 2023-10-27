e4c5295e6af05ca8f2def97c718b37fee5f87187
jiaju1998 <124156510+jiaju1998@users.noreply.github.com>
2023-10-27 16:56:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,10 +20,35 @@ import time
 #         schedule.run_pending
 if __name__ == '__main__':
     print('start of main func!')
+    stgIdx = sys.argv[1]
+    timePeriod = int(sys.argv[2])
+    if (stgIdx == '1'):
+        sum0, sum1, top20WinRateList, numCount = stg().stg1(timePeriod)
+        print('--------------------------------------------------------------------------------')
+        print('report of stg1')
+        print('统计的股票数 ' + str(numCount))
+        print('回测周期（天） ' + str(timePeriod))
+        print('进入策略次数 ' + str(sum0))
+        print('进入策略且挣钱次数 ' + str(sum1))
+        print('总成功率：' + str(sum1 / sum0))
+        print('top 20 stocks:')
+        for elem in top20WinRateList:
+            print(elem)
+        print('--------------------------------------------------------------------------------')
 
-    stg().stg2(20)
 
+    elif (stgIdx == '2'):
+        totalNum, winNum, stockNum = stg().stg2(timePeriod)
+        print('--------------------------------------------------------------------------------')
+        print('report of stg2')
+        print('统计的股票数 ' + str(stockNum))
+        print('进入策略次数 ' + str(totalNum))
+        print('进入策略且挣钱次数 ' + str(winNum))
+        print('总成功率：' + str(winNum / totalNum))
+        print('--------------------------------------------------------------------------------')
+
+    else:
+        print('no strategy support')
 
 
     print('end of main func')
-#github test
