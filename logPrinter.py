811e67c41509521e1fdbf541ce20ec7b461b1bb8
jiaju1998 <124156510+jiaju1998@users.noreply.github.com>
2023-10-30 17:19:11 +0800

    Add files via upload

---
+++ logPrinter.py
@@ -0,0 +1,5 @@
+def addLogLine(log, line):
+    return log + line + '\n'
+
+def addSeperate(log):
+    return log + '--------------------------------------------------------------------------------' + '\n'
